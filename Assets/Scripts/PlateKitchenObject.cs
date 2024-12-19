b4e53b71250f8cdf34d5214cb42a3bba2f9fe718
AhmetGjinofci <gjinofciahmet@gmail.com>
2024-12-19 23:44:17 +0100

    Add visual representation for completed plates

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -51,4 +51,9 @@ public class PlateKitchenObject : KitchenObject
         }
     }
 
+    public List<KitchenObjectSO> GetKitchenObjectSOList()
+    {
+        return kitchenObjectSOList;
+    }
+
 }
