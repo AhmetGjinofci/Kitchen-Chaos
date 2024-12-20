bd4daba80dc58605e6a7e0db58397f9d22f30af0
AhmetGjinofci <gjinofciahmet@gmail.com>
2024-12-20 17:52:01 +0100

    Implement a user interface for the delivery manager to show active orders

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -1,9 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DeliveryManager : MonoBehaviour
 {
+
+    public event EventHandler OnRecipeSpawned;
+    public event EventHandler OnRecipeCompleted;
+
 
     public static DeliveryManager Instance { get; private set; }
 
@@ -36,9 +41,11 @@ public class DeliveryManager : MonoBehaviour
 
             if (waitingRecipeSOList.Count < waitingRecipeMax)
             {
-                RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[Random.Range(0, recipeListSO.recipeSOList.Count)];
-                Debug.Log(waitingRecipeSO.recipeName);
+                RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[UnityEngine.Random.Range(0, recipeListSO.recipeSOList.Count)];
+
                 waitingRecipeSOList.Add(waitingRecipeSO);
+
+                OnRecipeSpawned?.Invoke(this, EventArgs.Empty);
             }
         }
     }
@@ -78,8 +85,11 @@ public class DeliveryManager : MonoBehaviour
                 if (plateContentsMatchesRecipe)
                 {
                     //Plate delivered the correct recipe!
-                    Debug.Log("Player delivered the correct recipe!");
+
                     waitingRecipeSOList.RemoveAt(i);
+
+
+                    OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
                     return;
                 }
             }
@@ -87,6 +97,11 @@ public class DeliveryManager : MonoBehaviour
 
         //No matches found!
         //Player did not deliver a correct recipe!
-        Debug.Log("Player did not deliver a correct recipe!");
     }
+
+    public List<RecipeSO> GetWaitingRecipeSOList()
+    {
+        return waitingRecipeSOList;
+    }
+
 }
