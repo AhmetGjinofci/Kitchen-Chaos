513a267ca7fd208d8d2fe015693278f0a4a083fe
AhmetGjinofci <gjinofciahmet@gmail.com>
2024-12-28 22:40:12 +0100

    Add options menu with adjustable audio levels

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
 
+
+    private const string PLAYER_PREFS_SOUND_EFFECTS_VOLUME = "SoundEffectsVolume";
+
+
+
     public static SoundManager Instance { get; private set; }
 
 
@@ -12,9 +17,15 @@ public class SoundManager : MonoBehaviour
     [SerializeField] private AudioClipRefsSO audioClipRefSO;
 
 
+
+    private float volume = 1f;
+
+
     private void Awake()
     {
         Instance = this;
+
+        volume = PlayerPrefs.GetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, 1f);
     }
 
 
@@ -69,15 +80,34 @@ public class SoundManager : MonoBehaviour
 
     }
 
-    private void PlaySound(AudioClip audioClip, Vector3 position, float volume = 1f)
+    private void PlaySound(AudioClip audioClip, Vector3 position, float volumeMultiplier = 1f)
     {
-        AudioSource.PlayClipAtPoint(audioClip, position, volume);
+        AudioSource.PlayClipAtPoint(audioClip, position, volumeMultiplier * volume);
 
     }
 
     public void PlayFootstepsSound(Vector3 position, float volume)
     {
         PlaySound(audioClipRefSO.footsteps, position, volume);
+    }
+
+
+    public void ChangeVolume()
+    {
+        volume += .1f;
+
+        if(volume > 1f)
+        {
+            volume = 0f;
+        }
+
+        PlayerPrefs.SetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, volume);
+        PlayerPrefs.Save();
+    }
+
+    public float GetVolume()
+    {
+        return volume;
     }
 
 }
