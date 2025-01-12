350f71625a4c503a1286cde1ecc0c65800b3ec0e
AhmetGjinofci <gjinofciahmet@gmail.com>
2025-01-12 23:19:43 +0100

    Fix minor bugs and inconsistencies throughout the project

--- Assets/Scripts/UI/DeliveryResultUI.cs
+++ Assets/Scripts/UI/DeliveryResultUI.cs
@@ -7,6 +7,9 @@ using UnityEngine.UI;
 public class DeliveryResultUI : MonoBehaviour
 {
 
+    private const string POPUP = "Popup";
+
+
     [SerializeField] private Image backgroundImage;
     [SerializeField] private Image iconImage;
     [SerializeField] private TextMeshProUGUI messageText;
@@ -17,14 +20,28 @@ public class DeliveryResultUI : MonoBehaviour
 
 
 
+    private Animator animator;
+
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+
     private void Start()
     {
         DeliveryManager.Instance.OnRecipeSuccess += DeliveryManager_OnRecipeSuccess;
         DeliveryManager.Instance.OnRecipeFailed += DeliveryManager_OnRecipeFailed;
+
+
+        gameObject.SetActive(false);
     }
 
     private void DeliveryManager_OnRecipeFailed(object sender, System.EventArgs e)
     {
+        gameObject.SetActive(true);
+        animator.SetTrigger(POPUP);
         backgroundImage.color = failedColor;
         iconImage.sprite = failedSprite;
         messageText.text = "DELIVERY\nFAILED";
@@ -32,6 +49,8 @@ public class DeliveryResultUI : MonoBehaviour
 
     private void DeliveryManager_OnRecipeSuccess(object sender, System.EventArgs e)
     {
+        gameObject.SetActive(true);
+        animator.SetTrigger(POPUP);
         backgroundImage.color = successColor;
         iconImage.sprite = successSprite;
         messageText.text = "DELIVERY\nSUCCESS";
